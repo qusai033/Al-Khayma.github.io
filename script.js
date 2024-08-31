f2f3fc1806b26af8252973b5b58baea2f0c52077
Qusai Almustafa <113052443+qusai033@users.noreply.github.com>
2024-08-31 12:52:27 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,1 @@
-document.addEventListener("DOMContentLoaded", function() {
-    // Logo Click Event
-    const logo = document.querySelector(".logo");
-    if (logo) {
-        logo.addEventListener("click", function(event) {
-            window.location.href = "index.html"; // Navigate back to the menu page
-        });
-    }
 
-    // Menu Button Click Event
-    const menuButton = document.getElementById("menu-button");
-    if (menuButton) {
-        menuButton.addEventListener("click", function(event) {
-            window.location.href = "index.html"; // Navigate back to the menu page
-        });
-    }
-});
