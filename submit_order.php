f03be68189c9a70f78625b7eb5b2a38a3760e757
Qusai Almustafa <113052443+qusai033@users.noreply.github.com>
2024-09-02 10:51:58 -0700

    Update submit_order.php

--- submit_order.php
+++ submit_order.php
@@ -1,24 +1,24 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $name = htmlspecialchars($_POST['name']);
-    $email = htmlspecialchars($_POST['email']);
-    $phone = htmlspecialchars($_POST['phone']);
-    $address = htmlspecialchars($_POST['address']);
-    $date = htmlspecialchars($_POST['date']);
-    $orderSummary = htmlspecialchars($_POST['order-summary']);
+    $name = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+    $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
+    $phone = filter_input(INPUT_POST, 'phone', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+    $address = filter_input(INPUT_POST, 'address', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+    $date = filter_input(INPUT_POST, 'date', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+    $orderSummary = filter_input(INPUT_POST, 'order-summary', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
 
-    $to = "qusai4business@gmail.com.com"; // Replace with your email
+    $to = "qusai4business@gmail.com"; // Ensure this is your correct email
     $subject = "New Order from $name";
     $message = "Name: $name\nEmail: $email\nPhone: $phone\nAddress: $address\nDelivery Date: $date\n\nOrder Summary:\n$orderSummary";
     
-    $headers = "From: no-reply@Al-Khayma.github.io\r\n"; // Use your domain email
+    $headers = "From: no-reply@Al-Khayma.github.io\r\n"; 
     $headers .= "Reply-To: $email\r\n";
     $headers .= "Content-Type: text/plain; charset=UTF-8\r\n";
 
     if (mail($to, $subject, $message, $headers)) {
-        echo "success";
+        echo "Thank you for your order. We will contact you shortly.";
     } else {
-        echo "error";
+        echo "There was an error processing your request. Please try again.";
     }
 }
 ?>
