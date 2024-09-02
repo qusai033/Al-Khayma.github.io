73ab94e3c51fbd9c01cf70bc1b7cb53ee703f4c7
Qusai Almustafa <113052443+qusai033@users.noreply.github.com>
2024-09-02 11:11:06 -0700

    Update submit_order.php

--- submit_order.php
+++ submit_order.php
@@ -1,4 +1,8 @@
+
 <?php
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $name = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
     $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
@@ -15,10 +19,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $headers .= "Reply-To: $email\r\n";
     $headers .= "Content-Type: text/plain; charset=UTF-8\r\n";
 
-    if (mail($to, $subject, $message, $headers)) {
-        echo "Thank you for your order. We will contact you shortly.";
+        if (mail($to, $subject, $message, $headers)) {
+            echo "Thank you for your order. We will contact you shortly.";
+        } else {
+            echo "There was an error processing your request. Please try again.";
+        }
     } else {
-        echo "There was an error processing your request. Please try again.";
+        echo "POST method not handled.";
     }
-}
 ?>
